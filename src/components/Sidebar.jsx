7fed46f7c7c31b0004858eeeaa9fdb21f56d2edf
Siam <ahmedsiam037@gmail.com>
2025-01-07 10:43:12 +1100

    app

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import { Outlet } from "react-router-dom";
+import Logo from "../../starter/components/Logo";
+import AppNav from "./AppNav";
+import styles from "./Sidebar.module.css";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <Outlet />
+
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WorldWide Inc.
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Sidebar;
